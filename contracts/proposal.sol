b243b305d5d861e584711e1d815959175177d8cd
anchi14 <sethi.anchal14@gmail.com>
2025-05-11 10:46:00 +0530

    update proposal

--- contracts/proposal.sol
+++ contracts/proposal.sol
@@ -2,6 +2,7 @@
 pragma solidity ^0.8.18;
 
 contract ProposalContract {
+    uint256 private counter; 
 
     struct Proposal {
         string description; // Description of the proposal
@@ -15,4 +16,13 @@ contract ProposalContract {
     }
 
     mapping(uint256 => Proposal) proposal_history; // Recordings of previous proposals
+
+
+
+
+    function create(string calldata _description,string calldata _title,  uint256 _total_vote_to_end) external {
+        counter += 1;
+        proposal_history[counter] = Proposal(_description, _title,0, 0, 0, _total_vote_to_end, false, true);
+}
+
 }
